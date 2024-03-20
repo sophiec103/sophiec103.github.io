fd8daf837d5e5da8fd469fe9c0ee34135067bb3c
sophiec103 <sophiechan103@gmail.com>
2024-03-19 23:28:00 -0400

    fix spacing and text

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import path from "path";
 const encode = Encode_Sans_Semi_Expanded({ subsets: ["latin"], weight: ["600"] });
 
 export default function Home() {
-  console.log(path.join(__dirname, 'src','css'))
   return (
     <main className="Home">
       <div className="content-wrapper">
@@ -14,7 +13,7 @@ export default function Home() {
           <h2 className="greeting">Hello!</h2>
           <h1 className={`name ${encode.className}`}>{"I'm Sophie Chan"}</h1>
           <p className="description">
-            {"I'm currently studying Computer Science at the University of Waterloo, with an anticipated graduation in 2026. I've had some great experiences working at Faire, CharityCAN, Adentro, and Atomic VC. I'm in the process of seeking internship opportunities for the Fall 2024 term."}
+            {"I'm currently studying Computer Science at the University of Waterloo, with an anticipated graduation in 2026. I've had some great experiences working at Faire, CharityCAN, Adentro, and Atomic VC. I'm seeking internship opportunities for the Fall 2024 term, and I look forward to making an impact wherever I go next!"}
           </p>
         </div>
         {/* <a href="https://emoji.gg/emoji/8771_blobheart">
