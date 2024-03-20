685b65b5146872ed065844e7352d2b3ac84c24c8
sophiec103 <sophiechan103@gmail.com>
2024-03-19 22:22:21 -0400

    progress to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,31 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./../css/home.scss";
+import { Encode_Sans_Semi_Expanded } from "next/font/google";
+
+const encode = Encode_Sans_Semi_Expanded({ subsets: ["latin"], weight: ["600"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+    <main className="Home">
+      <div className="content-wrapper">
+        <div className="text-wrapper">
+          <h2 className="greeting">Hello!</h2>
+          <h1 className={`name ${encode.className}`}>I'm Sophie Chan</h1>
+          <p className="description">
+            I'm studying Computer Science at the University of Waterloo, with an anticipated graduation in 2026.
+            I've had some great experiences working at Faire, CharityCAN, Adentro, and Atomic VC. 
+            I am currently seeking internship opportunities for the Fall 2024 term.
           </p>
-        </a>
+        </div>
+        {/* <a href="https://emoji.gg/emoji/8771_blobheart">
+          <img
+            src="https://cdn3.emoji.gg/emojis/8771_blobheart.png"
+            width="64px"
+            height="64px"
+            alt="blobheart"
+          />
+        </a> */}
+        <img src="/sophie.png" alt="Sophie profile picture" className="profile-picture"/>
       </div>
     </main>
   );
