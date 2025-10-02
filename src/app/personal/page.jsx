c7a4bb167fcc29d02995e7d3d6e8139ce9185b83
sophiec103 <sophiechan103@gmail.com>
2025-10-01 23:23:12 -0700

    add another

--- src/app/personal/page.jsx
+++ src/app/personal/page.jsx
@@ -6,20 +6,24 @@ import Gallery from "../../components/Gallery";
 
 const images = [
   {
-	src: "https://www.dropbox.com/scl/fi/bwg7js8q1yqco5cr5r6g6/family.jpg?rlkey=y54laojb49i2idvk5opf4c04e&st=wbxalpa6&raw=1",
-	description: "Family",
+	  src: "https://www.dropbox.com/scl/fi/bwg7js8q1yqco5cr5r6g6/family.jpg?rlkey=y54laojb49i2idvk5opf4c04e&st=wbxalpa6&raw=1",
+	  description: "Family",
   },
   {
-	src: "https://www.dropbox.com/scl/fi/wm0dtkjtgcxzoxgv4ic4n/life-is-tough-but-you-are-tougher.jpg?rlkey=xpnbc0qfvy7eps5rx659bw72k&st=5zre5231&raw=1",
-	description: "Life is tough but you are tougher",
+	  src: "https://www.dropbox.com/scl/fi/wm0dtkjtgcxzoxgv4ic4n/life-is-tough-but-you-are-tougher.jpg?rlkey=xpnbc0qfvy7eps5rx659bw72k&st=5zre5231&raw=1",
+	  description: "Life is tough but you are tougher",
   },
   {
-	src: "https://www.dropbox.com/scl/fi/ytestzahtvsur7byri1zd/halloween.jpg?rlkey=mra7nsi847s1kfcgvrdnpwr2d&st=3kxijojb&raw=1",
-	description: "Halloween",
+	  src: "https://www.dropbox.com/scl/fi/ytestzahtvsur7byri1zd/halloween.jpg?rlkey=mra7nsi847s1kfcgvrdnpwr2d&st=3kxijojb&raw=1",
+	  description: "Halloween",
   },
   {
-	src: "https://www.dropbox.com/scl/fi/2zs4fi797lrnh9lfppjfe/20210711-markville-grad.jpg?rlkey=opdmkxkckk9n3c6eebncmi6kb&st=0i4hw0wd&raw=1",
-	description: "Markville Graduation",
+	  src: "https://www.dropbox.com/scl/fi/2zs4fi797lrnh9lfppjfe/20210711-markville-grad.jpg?rlkey=opdmkxkckk9n3c6eebncmi6kb&st=0i4hw0wd&raw=1",
+	  description: "Markville Graduation",
+  },
+  {
+	  src: "https://www.dropbox.com/scl/fi/xifdktqk9ixj1erzzgduc/partner.jpg?rlkey=2xt3k2tjbio1v03zsszwqthk5&st=m7vav1ix&raw=1",
+	  description: "SO",
   },
 ];
 
